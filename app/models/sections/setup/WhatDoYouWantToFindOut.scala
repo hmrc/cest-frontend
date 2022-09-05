af962de4ea351849ff0dabe47433e437b2638ad5
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2022-09-05 16:09:13 +0100

    DL-7851: migrate WhatDoYouWantToFindOutView

--- app/models/sections/setup/WhatDoYouWantToFindOut.scala
+++ app/models/sections/setup/WhatDoYouWantToFindOut.scala
@@ -18,6 +18,8 @@ package models.sections.setup
 
 import models.{Enumerable, WithName}
 import play.api.libs.json._
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
 import viewmodels.{Radio, RadioOption}
 
 sealed trait WhatDoYouWantToFindOut
