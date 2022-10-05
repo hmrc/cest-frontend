8f9c5d63db7876cb2e03b36d49f2b86660bc2ec0
marjan <marjan.mubarok@digital.hmrc.gov.uk>
2022-10-05 14:57:04 +0100

    DL-7583 Refactor worker type view page

--- test/views/ViewSpecBaseNew.scala
+++ test/views/ViewSpecBaseNew.scala
@@ -111,8 +111,9 @@ trait ViewSpecBaseNew extends GuiceAppSpecBase {
     assert(radio.attr("name") == name, s"\n\nElement $id does not have name $name")
     assert(radio.attr("value") == value, s"\n\nElement $id does not have value $value")
     isChecked match {
-      case true => assert(radio.attr("checked") == "checked", s"\n\nElement $id is not checked")
-      case _ => assert(!radio.hasAttr("checked") && radio.attr("checked") != "checked", s"\n\nElement $id is checked")
+      case true => assert(radio.hasAttr("checked"), s"\n\nElement $id is not checked")
+      case _ => assert(!radio.hasAttr("checked"), s"\n\nElement $id is checked")
     }
   }
+
 }
