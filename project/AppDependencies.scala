68f26d30ab5f0d58a945ea7ddaa9a4cc0a463aff
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2023-06-07 17:10:55 +0100

    DL-10721 WIP Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,17 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "7.8.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "7.9.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "1.2.0",
     "uk.gov.hmrc"       %% "http-caching-client"              % "10.0.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.25.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "7.15.0",
     "com.typesafe.play" %% "play-json-joda"                   % "2.9.4",
     "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % "0.33.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "7.15.0",
     "uk.gov.hmrc"                  %% "service-integration-test"    % "1.3.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"     % "1.2.0",
     "org.mockito"                  %  "mockito-core"                % "5.3.1",
