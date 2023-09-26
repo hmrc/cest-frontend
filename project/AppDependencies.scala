a15b7a89f431fa7a487de4bfa753108988c5cc85
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2023-09-26 17:13:24 +0100

    DL-11522 WIP Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "6.8.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "7.19.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "1.1.0",
-    "uk.gov.hmrc"       %% "http-caching-client"              % "9.6.0-play-28",
+    "uk.gov.hmrc"       %% "http-caching-client"              % "10.0.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.25.0",
     "com.typesafe.play" %% "play-json-joda"                   % "2.9.4",
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0",
     "uk.gov.hmrc"                  %% "service-integration-test"    % "1.3.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"     % "1.1.0",
-    "org.mockito"                  %  "mockito-core"                % "4.11.0",
+    "org.mockito"                  %  "mockito-core"                % "5.5.0",
     "org.scalatestplus"            %% "scalatestplus-mockito"       % "1.0.0-M2",
     "org.scalatestplus"            %% "scalacheck-1-15"             % "3.2.11.0",
     "org.jsoup"                    %  "jsoup"                       % "1.15.4",
