4f2204c74f6fcfa3692078cc40c2ec92d106eede
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2023-09-27 15:42:59 +0100

    DL-11522 Dependency Updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "7.19.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "1.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % "7.20.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"               % "1.3.0",
     "uk.gov.hmrc"       %% "http-caching-client"              % "10.0.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "5.25.0",
-    "com.typesafe.play" %% "play-json-joda"                   % "2.9.4",
-    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % "0.29.0-play-28"
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"       % "7.22.0",
+    "com.typesafe.play" %% "play-json-joda"                   % "2.10.1",
+    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % "0.33.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
